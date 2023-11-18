0bdd86b799d46d16fcccef02013919b352b6c17b
madhav <eragon.cube@outlook.com>
2023-11-18 23:36:31 +0530

    Changing main branch to display only teaser

--- scripts/index.js
+++
@@ -1,9 +0,0 @@
-const navBar = document.querySelector('.nav');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 50) {
-    navBar.classList.add('nav--dark');
-  } else {
-    navBar.classList.remove('nav--dark');
-  }
-});
